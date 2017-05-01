5796873f5ae0ea019736dceac76eeccb3cff33db
Peter Liu <onhout@gmail.com>
2017-05-01 12:23:07 -0700

    added commands for admin user and admin support

--- mina/admin.py
+++ mina/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking, Files
+
+
+class BookingAdmin(admin.ModelAdmin):
+    model = Booking
+    list_display = [
+        'user',
+        'book_type',
+        'class_location',
+        'start',
+        'end',
+        'transaction_amount',
+        'created_at',
+    ]
+
+
+class FilesAdmin(admin.ModelAdmin):
+    model = Files
+    list_display = [
+        'to_user',
+        'file',
+        'created_at',
+    ]
+
+
+admin.site.register(Booking, BookingAdmin)
+
+admin.site.register(Files, FilesAdmin)
