a669b47fdacb5b52de8747aaeba3fad6817d5a78
Peter Liu <onhout@gmail.com>
2017-05-12 11:09:37 -0700

    Always forgot to turn off debug mode

--- Minalanguage/settings.py
+++ Minalanguage/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '=%$oh_4*5$-6h%s(_%&7k&*$9mauf2lh2g=l$+yjrxa-l&^vr('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # SET IT TO FALSE FOR PROD MODE
 USE_TZ = False
 
